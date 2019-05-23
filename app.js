6af6b7bd8f4011ec0941838d5222c38ed345e778
Ryan Boehm <ryan.w.boehm@gmail.com>
2019-05-22 23:50:34 -0400

    removed wrapper elements, completed draggable list functionality

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
 jQuery(document).ready(function() {
-  var item = $('.items-wrap .item');
-  item.draggable({
-    containment: '.items-wrap .items',
-    cursor: 'move',
-    snap: '.items-wrap .items'
-  } );
+  $('.items-wrap .items').sortable();
 });
